e88f942e5e5c6909a1ef918f19196772d3c14a22
HRMLPT-134\Mauro.Spinosi <mauro.spinosi@hrm.group>
2025-07-29 16:27:28 +0200

    added fontSize property

--- blocks/tpd-rich-text/tpd-rich-text.js
+++ blocks/tpd-rich-text/tpd-rich-text.js
@@ -4,4 +4,17 @@ import { moveInstrumentation } from '../../scripts/scripts.js';
 export default function decorate(block) {
   const children = [...block.children];
 
+  const paragraph = children[0];
+  const FontSize = children[1]?.textContent.trim().toLowerCase() || '';
+
+  if (['Font-size-xl', 'Font-size-l', 'Font-size-m', 'Font-size-s', 'Font-size-xs', 'Font-size-xxs'].includes(FontSize)) {
+    paragraph.classList.add(`${FontSize}`);
+  }
+
+  paragraph.setAttribute('data-aue-label', 'Paragrafo');
+  paragraph.setAttribute('data-aue-type', 'text');
+
+  moveInstrumentation(paragraph);
+  moveInstrumentation(block);
+
 }
