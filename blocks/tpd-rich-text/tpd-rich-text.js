407cd0bc4a73442111822330246f0bb2d06ec1e7
HRMLPT-134\Mauro.Spinosi <mauro.spinosi@hrm.group>
2025-07-29 15:38:58 +0200

    added tpd-rich-text component

---
+++ blocks/tpd-rich-text/tpd-rich-text.js
@@ -0,0 +1,7 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
+export default function decorate(block) {
+  const children = [...block.children];
+
+}
