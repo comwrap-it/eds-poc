ef0c501d0e3b12699be6218d8be97cee134d544e
HRMLPT-134\Mauro.Spinosi <mauro.spinosi@hrm.group>
2025-07-29 17:21:51 +0200

    added color property to richtext

--- blocks/tpd-rich-text/tpd-rich-text.js
+++ blocks/tpd-rich-text/tpd-rich-text.js
@@ -5,11 +5,19 @@ export default function decorate(block) {
   const children = [...block.children];
 
   const paragraph = children[0];
-  const FontSize = children[1]?.textContent.trim().toLowerCase() || '';
+  const FontSize = children[1]?.textContent.trim() || '';
 
   if (['Font-size-xl', 'Font-size-l', 'Font-size-m', 'Font-size-s', 'Font-size-xs', 'Font-size-xxs'].includes(FontSize)) {
     paragraph.classList.add(`${FontSize}`);
   }
+
+  const textColor = children[2]?.textContent.trim() || '';
+  if (textColor) {
+    paragraph.style.color = textColor;
+  }
+
+  children[1]?.remove();
+  children[2]?.remove();
 
   paragraph.setAttribute('data-aue-label', 'Paragrafo');
   paragraph.setAttribute('data-aue-type', 'text');
