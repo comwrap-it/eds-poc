7d61829dc808f9b6ff0c35b2458d001d8450a21f
HRMLPT-134\Mauro.Spinosi <mauro.spinosi@hrm.group>
2025-07-29 11:47:51 +0200

    fixed problem on title color property

--- blocks/section-title/section-title.js
+++ blocks/section-title/section-title.js
@@ -17,6 +17,7 @@ export default function decorate(block) {
   }
 
   children[1]?.remove();
+  children[2]?.remove();
 
   heading.setAttribute('data-aue-label', 'Titolo');
   heading.setAttribute('data-aue-type', 'text');
