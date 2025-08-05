acb634d5ff10089cbe23f4b1ac6c3a3da82ce5f8
valbini <v.albini@reply.it>
2025-08-05 11:57:48 +0200

    Update dev-config.js

--- config/dev-config.js
+++ config/dev-config.js
@@ -1,6 +1,7 @@
 export const DEV_CONFIG = {
   domain: 'http://localhost:4502',
   cloudPublishDomain: 'https://publish-p42403-e1312991.adobeaemcloud.com',
+  cloudAuthorDomain: 'https://author-p42403-e1312991.adobeaemcloud.com',
   username: 'admin',
   password: 'admin',
   isLocalDevelopment: window.location.port === '3000'
@@ -10,8 +11,20 @@ export function getAuthHeader() {
   return 'Basic ' + btoa(`${DEV_CONFIG.username}:${DEV_CONFIG.password}`);
 }
 
+export function isAuthorInstance() {
+  // Controlla l'URL corrente
+  const currentUrl = window.location.href;
+  return currentUrl.includes('author-');
+}
+
 export function getGraphQLEndpoint(path) {
-  return DEV_CONFIG.isLocalDevelopment 
-    ? `${DEV_CONFIG.domain}${path}`
-    : `${DEV_CONFIG.cloudPublishDomain}${path}`;
+  if (DEV_CONFIG.isLocalDevelopment) {
+    return `${DEV_CONFIG.domain}${path}`;
+  }
+  
+  const baseUrl = isAuthorInstance() 
+    ? DEV_CONFIG.cloudAuthorDomain 
+    : DEV_CONFIG.cloudPublishDomain;
+    
+  return `${baseUrl}${path}`;
 }
