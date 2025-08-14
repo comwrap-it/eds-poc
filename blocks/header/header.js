8891f4f221bc694535b8a17fb974f1da4695f119
HRMLPT-134\Mauro.Spinosi <mauro.spinosi@hrm.group>
2025-08-14 18:17:11 +0200

    restored header component

---
+++ blocks/header/header.js
@@ -0,0 +1,20 @@
+import { getMetadata } from '../../scripts/aem.js';
+import { loadFragment } from '../fragment/fragment.js';
+
+/**
+ * loads and decorates the header
+ * @param {Element} block The header block element
+ */
+export default async function decorate(block) {
+  // load header as fragment
+  const headerMeta = getMetadata('header');
+  const headerPath = headerMeta ? new URL(headerMeta, window.location).pathname : '/header';
+  const fragment = await loadFragment(headerPath);
+
+  // decorate footer DOM
+  block.textContent = '';
+  const header = document.createElement('div');
+  while (fragment.firstElementChild) header.append(fragment.firstElementChild);
+
+  block.append(header);
+}
