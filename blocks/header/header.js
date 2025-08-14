9975aa02829dded207065f29341975590cb5fc6b
HRMLPT-134\Mauro.Spinosi <mauro.spinosi@hrm.group>
2025-08-14 18:36:50 +0200

    fixed err in header .js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -8,10 +8,10 @@ import { loadFragment } from '../fragment/fragment.js';
 export default async function decorate(block) {
   // load header as fragment
   const headerMeta = getMetadata('header');
-  const headerPath = headerMeta ? new URL(headerMeta, window.location).pathname : '/header';
+  const headerPath = headerMeta ? new URL(headerMeta, window.location).pathname : '/nav';
   const fragment = await loadFragment(headerPath);
 
-  // decorate footer DOM
+  // decorate header DOM
   block.textContent = '';
   const header = document.createElement('div');
   while (fragment.firstElementChild) header.append(fragment.firstElementChild);
